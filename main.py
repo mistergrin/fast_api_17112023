0993f5e26a10f46978b5e03ecfa40d6d4306e3c4
“Mister_Grin” <vanogrishin2007@gmail.com>
2024-01-20 17:34:54 +0100

    first get endpoint

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get('/')
+def root() -> dict:
+    return {'try': "ok", "count": 10, "bool": True}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True, host='0.0.0.0', port=8000)
